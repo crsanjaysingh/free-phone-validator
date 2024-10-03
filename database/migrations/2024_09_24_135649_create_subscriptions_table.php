5f733f83e282830ea62e241d9631ddc591afc536
sanjay0 <sanjay.singh@infrapayments.com>
2024-10-03 20:07:08 +0530

    Fixed nullable schema issue

--- database/migrations/2024_09_24_135649_create_subscriptions_table.php
+++ database/migrations/2024_09_24_135649_create_subscriptions_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
       $table->foreignId('user_id')->constrained()->onDelete('cascade');
       $table->foreignId('plan_id')->constrained('plans')->onDelete('cascade');
       $table->integer('lookups_remaining')->default(0);
-      $table->timestamp('started_at');
-      $table->timestamp('ends_at');
+      $table->timestamp('started_at')->nullable();
+      $table->timestamp('ends_at')->nullable();
       $table->boolean('is_recurring')->default(true);
       $table->timestamp('next_billing_date')->nullable();
       $table->boolean('status')->default(true);
